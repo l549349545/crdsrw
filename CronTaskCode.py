1667037759acd4b7357b64a46e327692e484ac1a
l549349545 <306941068@qq.com>
2021-06-05 03:19:59 +0800

    Update CronTaskCode.py

--- CronTaskCode.py
+++ CronTaskCode.py
@@ -3,7 +3,7 @@ import json
 
 def run():
         payload = json.dumps({"ref": "main"})
-        header = {'Authorization': 'token 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',
+        header = {'Authorization': 'token **********',
                   "Accept": "application/vnd.github.v3+json"}
         response_decoded_json = requests.post(
             f'https://api.github.com/repos/l549349545/crdsrw/actions/workflows/9310500/dispatches',
