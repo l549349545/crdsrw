5d5fcf9b9cb5e6d1cbc86d1e9860f61b66e7b79c
l549349545 <306941068@qq.com>
2021-06-22 20:08:57 +0800

    Update mrds.py

--- mrds.py
+++ mrds.py
@@ -130,19 +130,19 @@ def run():
 
     #企业微信推送
     msg = msg.replace('\n\n', '\n')
-    push = WXPusher(pushusr,msg)
-    push.send_message()
+    #push = WXPusher(pushusr,msg)
+    #push.send_message()
 
     #酷推推送
-    requests.get(coolpushurl, params={"c": "不愿意透露姓名的好心人王富贵提供\n"+data})
+    #requests.get(coolpushurl, params={"c": "不愿意透露姓名的好心人王富贵提供\n"+data})
     
     strBoss=strBoss + "\n更新时间(UTC):"+datetime.datetime.strftime(datetime.datetime.now() ,'%Y-%m-%d %H:%M:%S')
     strJc=strJc + "\n更新时间(UTC):"+datetime.datetime.strftime(datetime.datetime.now() ,'%Y-%m-%d %H:%M:%S')
     strZc=strZc + "\n更新时间(UTC):"+datetime.datetime.strftime(datetime.datetime.now() ,'%Y-%m-%d %H:%M:%S')    
     retmsg=data+ "更新时间(UTC):"+datetime.datetime.strftime(datetime.datetime.now() ,'%Y-%m-%d %H:%M:%S')
     strPt=strPt+ "\n更新时间(UTC):"+datetime.datetime.strftime(datetime.datetime.now() ,'%Y-%m-%d %H:%M:%S')
-    
-    model={"大使任务：\n"+retmsg,"世界BOSS：\n"+strBoss,"泊星剧场：\n"+strJc,"周常任务：\n"+strZc,"噬渊爬塔：\n"+strPt} #数据
+        
+    model = {'DSRW':"大使任务：\n"+retmsg,'SJBS':"世界BOSS：\n"+strBoss,'BXJC':"泊星剧场：\n"+strJc,'ZCRW':"周常任务：\n"+strZc,'SYPT':"噬渊爬塔：\n"+strPt}
     with open("./hmm.json",'w',encoding='utf-8') as json_file:
        json.dump(model,json_file,ensure_ascii=False, default=set_default)
     
