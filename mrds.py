8a522125f51ba1fda439c6164f61384829d772fe
l549349545 <306941068@qq.com>
2021-07-01 23:42:03 +0800

    Update mrds.py

--- mrds.py
+++ mrds.py
@@ -51,6 +51,20 @@ d.update({'A Calling in Ardenweald': '使命：炽蓝仙野：在炽蓝仙野完
 d.update({'A Calling in Maldraxxus': '使命：玛卓克萨斯：在玛卓克萨斯完成3个世界任务'})
 d.update({'A Calling in Revendreth': '使命：雷文德斯：在雷文德斯完成3个世界任务'})
 
+
+cz = {'1': "强韧、崩裂、火山"}
+cz.update({'2': '残暴、怨毒、重伤'})
+cz.update({'3': '强韧、激励、风雷'})
+cz.update({'4': '残暴、鼓舞、死疽'})
+cz.update({'5': '强韧、血池、震荡'})
+cz.update({'6': '残暴、暴怒、易爆'})
+cz.update({'7': '强韧、怨毒、火山'})
+cz.update({'8': '残暴、激励、死疽'})
+cz.update({'9': '强韧、鼓舞、风雷'})
+cz.update({'10': '残暴、崩裂、易爆'})
+cz.update({'11': '强韧、血池、重伤'})
+cz.update({'12': '残暴、暴怒、震荡'})
+
 def set_default(obj):
     if isinstance(obj, set):
          return list(obj)
@@ -66,6 +80,55 @@ def run():
        #data = r.text
        sel=etree.HTML(r.text)    
        
+       #获取全谱系驱魔师
+       con=sel.xpath('normalize-space(//div[@id="EU-group-familyExorcist"])')
+       strQtx = ''
+       for i in con:
+        strQtx += i
+        
+       #获取执事者金色宝箱
+       con=sel.xpath('normalize-space(//div[@id="EU-group-steward-of-the-day"])')
+       strJsbx = ''
+       for i in con:
+        strJsbx += i
+        
+       #获取噬渊狩猎小队
+       con=sel.xpath('normalize-space(//div[@id="EU-group-beastwarrens-hunts"])')
+       strSlxd = ''
+       for i in con:
+        strSlxd += i
+        
+       #获取镜子
+       con=sel.xpath('normalize-space(//div[@id="EU-group-venthyr-broken-mirrors"])')
+       strJz = ''
+       for i in con:
+        strJz += i
+        
+       #获取世界事件
+       con=sel.xpath('normalize-space(//div[@id="EU-group-holiday"])')
+       strSjsj = ''
+       for i in con:
+        strSjsj += i
+        
+       #获取名望上限
+       con=sel.xpath('normalize-space(//div[@id="EU-group-renown"])')
+       strMwsx = ''
+       for i in con:
+        strMwsx += i
+
+       #获取勇气上限
+       con=sel.xpath('normalize-space(//div[@id="EU-group-valor"])')
+       strYqsx = ''
+       for i in con:
+        strYqsx += i
+
+        #获取词缀
+       zspp = (datetime.datetime.now()+ datetime.timedelta(hours=24)).isocalendar()[1] % 12 + 4 #周数取12余+4
+       zspp_next = zspp + 1
+       strCzinfo = "本周："+str(zspp)+"\n下周："+str(zspp_next)
+       for key in cz:
+        strCzinfo = strCzinfo.replace(key, cz[key])
+
        #获取世界BOSS
        con=sel.xpath('normalize-space(//div[@id="EU-group-epiceliteworldsl"])')
        strBoss = ''
@@ -127,7 +190,7 @@ def run():
     except Exception as err:
         msg = '发生错误',err
     else:
-        msg = data
+        msg = data +"\n\n剧场："+strJc  +"\n\n镜子："+strJz +"\n\n执事者金色宝箱："+strJsbx +"\n\n全谱系驱魔师："+strQtx
 
     #企业微信推送
     msg = msg.replace('\n\n', '\n')
@@ -136,26 +199,46 @@ def run():
 
     #酷推推送
     current_time = (datetime.datetime.now() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')   
-    requests.get(coolpushurl, params={"c": "机器人查询指令：大使、世界BOSS、剧场、周常、爬塔\n"+data+"\n网站查看：http://baimiao.work"+"\n"+current_time})
+    requests.get(coolpushurl, params={"c": "机器人查询指令：大使、世界BOSS、剧场、周常、爬塔、全谱系、金色宝箱、狩猎小队、镜子、世界事件、名望、勇气、词缀\n"+data+"\n网站查看：http://baimiao.work"+"\n"+current_time})
 
     #POST发布文章
     conurl = "http://baimiao.work/action/import"
-    conttext="大使任务：\n"+data+"\n\n\n\n世界BOSS(明日)：\n"+strBoss+"\n\n\n\n泊星剧场(明日)：\n"+strJc+"\n\n\n\n周常任务(明日)：\n"+strZc+"\n\n\n\n噬渊爬塔(明日)：\n"+strPt;
+    conttext="大使任务：\n"+data +"\n\n\n\n世界BOSS(明日)：\n"+strBoss +"\n\n\n\n泊星剧场(明日)：\n"+strJc+"\n\n\n\n周常任务(明日)：\n"+strZc+"\n\n\n\n噬渊爬塔(明日)：\n"+ strPt+"\n\n\n\n全谱系驱魔师(明日)：\n"+strQtx+"\n\n\n\n执事者金色宝箱(明日)：\n"+strJsbx+"\n\n\n\n噬渊狩猎小队(明日)：\n"+strSlxd+"\n\n\n\n温西尔镜子(明日)：\n"+strJz+"\n\n\n\n世界事件(明日)：\n"+strSjsj+"\n\n\n\n名望上限(明日)：\n"+strMwsx+"\n\n\n\n勇气上限(明日)：\n"+strYqsx+"\n\n\n\n大秘境词缀(明日)：\n"+strCzinfo;
     contdata = {"title":"大使任务 更新时间(UTC+8):"+current_time,"text":conttext,"key":"ob7hww6fs2e4xo9lltzewcpok5","mid":array([6])}
     requests.post(url=conurl,data=contdata)
     
-    #保存本地json
+    #保存本地json    
+    retmsg=data+ "更新时间(UTC+8):"+current_time
     strBoss=strBoss + "\n更新时间(UTC+8):"+current_time
     strJc=strJc + "\n更新时间(UTC+8):"+current_time
     strZc=strZc + "\n更新时间(UTC+8):"+current_time    
-    retmsg=data+ "更新时间(UTC+8):"+current_time
-    strPt=strPt+ "\n更新时间(UTC+8):"+current_time
+    strPt=strPt+ "\n更新时间(UTC+8):"+current_time 
+    strQtx=strQtx+ "\n更新时间(UTC+8):"+current_time 
+    strJsbx=strJsbx+ "\n更新时间(UTC+8):"+current_time 
+    strSlxd=strSlxd+ "\n更新时间(UTC+8):"+current_time 
+    strJz=strJz+ "\n更新时间(UTC+8):"+current_time 
+    strSjsj=strSjsj+ "\n更新时间(UTC+8):"+current_time 
+    strMwsx=strMwsx+ "\n更新时间(UTC+8):"+current_time 
+    strYqsx=strYqsx+ "\n更新时间(UTC+8):"+current_time 
+    strCzinfo=strCzinfo+ "\n更新时间(UTC+8):"+current_time
         
-    model = {'DSRW':"大使任务：\n"+retmsg,'SJBS':"世界BOSS(明日)：\n"+strBoss,'BXJC':"泊星剧场(明日)：\n"+strJc,'ZCRW':"周常任务(明日)：\n"+strZc,'SYPT':"噬渊爬塔(明日)：\n"+strPt}
+    model = {'DSRW':"大使任务：\n"+retmsg,
+             'SJBS':"世界BOSS(明日)：\n"+strBoss,
+             'BXJC':"泊星剧场(明日)：\n"+strJc,
+             'ZCRW':"周常任务(明日)：\n"+strZc,
+             'SYPT':"噬渊爬塔(明日)：\n"+strPt,
+             'QPXQMS':"全谱系驱魔师(明日)：\n"+strQtx,
+             'ZSZJSBX':"执事者金色宝箱(明日)：\n"+strJsbx,
+             'SYSLXD':"噬渊狩猎小队(明日)：\n"+strSlxd,
+             'WXEJZ':"温西尔镜子(明日)：\n"+strJz,
+             'SJSJ':"世界事件(明日)：\n"+strSjsj,
+             'MWSX':"名望上限(明日)：\n"+strMwsx,
+             'YQSX':"勇气上限(明日)：\n"+strYqsx,
+             'DMJCZ':"大秘境词缀(明日)：\n"+strCzinfo}
     with open("./hmm.json",'w',encoding='utf-8') as json_file:
        json.dump(model,json_file,ensure_ascii=False, default=set_default)
     
-    print(msg)
+    print(conttext)
     return retmsg
  
 def main_handler(event, context):
@@ -173,7 +256,7 @@ class WXPusher:
         self.img_url = img_url
         if usr is None:
             usr = '@all'
-        self.title = '不愿意透露姓名的好心人王富贵提供：'
+        self.title = '白描描描描描描描描描描提供：'
         self.usr = usr
         self.msg = desp
 
