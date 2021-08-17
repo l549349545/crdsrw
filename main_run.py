67726e49d1992e673397f6046bfebcfdce9fdb0e
l549349545 <306941068@qq.com>
2021-08-18 00:03:37 +0800

    Update main_run.py

--- main_run.py
+++ main_run.py
@@ -77,12 +77,11 @@ def run():
        strQtx = ''
        for i in con:
         strQtx += i
-        
+       strQtx = strQtx +"\n\n"
        con=sel.xpath('normalize-space(//div[@id="EU-group-familiar"])')
        for i in con:
         strQtx += i
-        
-        
+       strQtx = strQtx +"\n\n"
        con=sel.xpath('normalize-space(//div[@id="EU-group-menagerie"])')
        for i in con:
         strQtx += i
